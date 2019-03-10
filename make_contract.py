baa229f2610662c4d9f85b144c8aa7b953a97d5a
Eric D. Hiller <eric@hiller.pro>
2019-03-10 15:53:52 -0400

    initial commit of charts experimental

---
+++ make_contract.py
@@ -0,0 +1,55 @@
+import datetime
+import random
+from mypy_extensions import TypedDict
+from typing import List
+import json
+
+DAYS = 400
+START = datetime.datetime.now()
+MAX = 200
+
+
+class DayRecord(TypedDict):
+    """ Container for days trading """
+
+    date: datetime.date
+    open: float
+    close: float
+    high: float
+    low: float
+    volume: float
+    symbol: str
+
+
+records: List[DayRecord] = []
+
+for i in range(int(START.timestamp()), int(START.timestamp()) + (30 * 86400), 86400):
+    base = 0
+    vals: List[float] = []
+    for _ in range(4):
+        vals.append( random.randrange(base * 100, MAX * 100) / 100 )
+        base = int(vals[len(vals) - 1])
+        # print(f"base={base}")
+    # print(vals)
+    # exit()
+    _low, _open , _close, _high = vals
+    if random.randint(0, 1):
+        _open = vals[2]
+        _close = vals[1]
+    records.append( DayRecord(
+        date=datetime.date.fromtimestamp(i),
+        open=_open,
+        close=_close,
+        high=_high,
+        low=_low,
+        volume=random.randint(0, 500000),
+        symbol="ZCN9"
+    ) )
+
+def dateformatter(o):
+    if isinstance(o, (datetime.date, datetime.datetime)):
+        return o.isoformat()
+
+from pprint import pprint
+print(json.dumps(records , default=dateformatter , indent=2 ) )
+
---
+++ notes.md
@@ -0,0 +1,20 @@
+# Notes on Vega spec
+
+## Formatting
+
+* [d3 number format](https://github.com/d3/d3-format#locale_format)
+* [d3 date format](https://github.com/d3/d3-time-format#locale_format)
+
+* [Vega UTC time scale](https://vega.github.io/vega/docs/scales/#utc)
+* In Vega , how to [Parse data field as format](https://vega.github.io/vega/docs/data/#format)
+
+## Cursors
+
+* can be set on `Mark`s with `cursor` property
+* see [CSS Valid Cursor Values](https://developer.mozilla.org/en-US/docs/Web/CSS/cursor#Values)
+
+## To-Do
+
+* [ ] Dynamic Axes (date/time)
+* [ ] Highlight in tooltip the current datum property (high if on high point, low point, open, close, etc.)
+* [ ] Format date in tooltip
